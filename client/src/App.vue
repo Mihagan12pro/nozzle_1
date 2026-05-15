df7e6d6ff0728f0e6583f6a267d2721cce18fc97
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-05-15 11:41:49 +0300

    feat: add initial design

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav>
+      <h3>Расчет сопла Лаваля</h3>
+      <button class="btn">
+        Новый расчет
+      </button>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 10px;
+  margin: 0;
+  background-color: rgba(0, 0, 0, 0.89);
+}
+
+h3 {
+  text-align: center;
+  color: white;
+  font-size: 30px;
+  padding: 5px;
+  padding-right: 50px;
+  display: inline-block;
+}
+
+button {
+  color: #fff;
+  background: linear-gradient(to right, #11998e, #38ef7d);
+  outline: none;
+  border: 0;
+  color: #fff;
+  padding: 10px 20px;
+  text-transform: uppercase;
+  border-radius: 2px;
+  cursor: pointer;
+  position: relative;
+  width: 200px;
+  display: inline-block;
+}
+
+.btn {
+  margin-left: 16rem;
+}
+
+button:hover {
+  box-shadow: 3px 4px 20px #828282;
 }
 </style>
